09c88bd4460c1989a753994958cb0b3981a19874
Steve McCoy <mccoyst@gmail.com>
2012-01-16 23:01:56 -0500

    I broke something, so I must be on the right track.

---
+++ src/edu/unh/cs/tact/Checker.java
@@ -0,0 +1,9 @@
+// Copyright © 2012 Steve McCoy under the MIT license.
+package edu.unh.cs.tact;
+
+class Checker{
+	public static Object check(Object o){
+		System.err.println("Incredible!");
+		return o;
+	}
+}
--- src/edu/unh/cs/tact/Main.java
+++ src/edu/unh/cs/tact/Main.java
@@ -1,23 +1,56 @@
 // Copyright © 2012 Steve McCoy under the MIT license.
 package edu.unh.cs.tact;
 
+import java.util.*;
+import org.apache.bcel.*;
 import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
 
 class Main{
+	int someField;
+
 	public static void main(String[] args) throws Exception{
+		Main man = new Main();
+		man.someField = 666;
+
 		JavaClass jc = new ClassParser(args[0]).parse();
 
+		ClassGen cg = new ClassGen(jc);
+		InstructionFactory f = new InstructionFactory(cg);
+
+		for(Method m : jc.getMethods()){
+			Code c = m.getCode();
+			if(c == null)
+				continue;
+			byte[] bc = m.getCode().getCode();
+			InstructionList il = new InstructionList(bc);
+
+			for(InstructionHandle h : iter(il)){
+				if(h.getInstruction() instanceof PUTFIELD)
+					il.insert(h,
+						f.createInvoke("edu.unh.cs.tact.Checker",
+							"check",
+							Type.OBJECT,
+							new Type[]{ Type.OBJECT },
+							Constants.INVOKESTATIC
+						));
+			}
+
+			c.setCode(il.getByteCode());
+			System.out.println(c);
+		}
+
 		System.out.println(jc);
-		printCode(jc.getMethods());
 	}
 
-	public static void printCode(Method[] methods) {
-		for(int i=0; i < methods.length; i++) {
-			System.out.println(methods[i]);
-
-			Code code = methods[i].getCode();
-			if(code != null)
-				System.out.println(code);
-		}
+	static Iterable<InstructionHandle> iter(final InstructionList il){
+		return new Iterable<InstructionHandle>(){
+			@SuppressWarnings("unchecked")
+			public Iterator<InstructionHandle> iterator(){
+				// Why does this warn?
+				// http://commons.apache.org/bcel/apidocs/org/apache/bcel/generic/InstructionList.html#iterator()
+				return il.iterator();
+			}
+		};
 	}
 }
