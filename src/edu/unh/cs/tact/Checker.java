2abf0853383ce7fffdae8f3c1f769aa32c74eea8
Steve McCoy <mccoyst@gmail.com>
2012-01-30 12:51:48 -0500

    Implement junky version of ownership check.

--- src/edu/unh/cs/tact/Checker.java
+++ src/edu/unh/cs/tact/Checker.java
@@ -1,8 +1,25 @@
 // Copyright © 2012 Steve McCoy under the MIT license.
 package edu.unh.cs.tact;
 
+import java.util.*;
+
 class Checker{
-	public static void check(Object o){
-		System.err.println("Incredible!");
+	private static Map<Object, Thread> owners = new HashMap<Object, Thread>();
+
+	public static synchronized void check(Object o){
+		Thread ct = Thread.currentThread();
+
+		if(!owners.containsKey(o)){
+			owners.put(o, ct);
+			System.err.printf("Added %s -> %s\n", o, ct);
+			return;
+		}
+
+		if(owners.get(o).equals(ct)){
+			System.err.printf("OK access (%s -> %s)\n", o, ct);
+			return;
+		}
+
+		throw new IllegalAccessError(String.format("BAD access (%s -> %s)\n", o, ct));
 	}
 }
